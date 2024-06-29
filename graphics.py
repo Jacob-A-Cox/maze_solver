fc9b664686d804477e577df22ce23f96345db030
Jacob-A-Cox <jacob.a.cox21@gmail.com>
2024-06-29 04:01:48 -0700

    4.1 update graphics.py, main.py | add main.sh

--- graphics.py
+++ graphics.py
@@ -14,6 +14,10 @@ class Window:
         self.__root.update_idletasks()
         self.__root.update()
 
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+
     def wait_for_close(self):
         self.__running = True
         while self.__running:
@@ -22,3 +26,18 @@ class Window:
 
     def close(self):
         self.__running = False
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(canvas, fill_color="black"):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 
 def main():
     win = Window(800, 600)
+
+    point1 = Point(40,40)
+    point2 = Point(80,80)
+    line1 = Line(point1, point2)
+
+    point3 = Point(100,100)
+    point4 = Point(120,120)
+    line2 = Line(point3, point4)
+
+    win.draw_line(line1)
+    win.draw_line(line2, "red")
+
     win.wait_for_close()
 
 
