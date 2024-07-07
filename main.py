7a77000cabe571861bbc87948bbdd9496a7cee0a
Jacob-A-Cox <jacob.a.cox21@gmail.com>
2024-07-06 21:03:37 -0700

    Final Submissions

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
-from graphics import Window, Point, Line
-from cell import Cell
+from graphics import Window
+from maze import Maze
 
 
 def main():
-    win = Window(800, 600)
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
 
-    point1 = Point(40,40)
-    point2 = Point(80,80)
-    line1 = Line(point1, point2)
-
-    point3 = Point(100,100)
-    point4 = Point(120,120)
-    line2 = Line(point3, point4)
-
-    win.draw_line(line1)
-    win.draw_line(line2, "red")
-
-    c1 = Cell(win)
-    c2 = Cell(win)
-
-    c1.draw(300, 300, 400, 400)
-    c2.has_bottom_wall = False
-    c2.draw(400, 400, 500, 500)
-
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+    maze.solve()
     win.wait_for_close()
 
 
